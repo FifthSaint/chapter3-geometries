a5c0165fec5efe9bf7be30177c10f06324e2914b
Yewoon Bae <moonjy@khu.ac.kr>
2018-08-31 17:55:29 +0900

    updated

--- code.R
+++ code.R
@@ -16,7 +16,7 @@ ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) +
 ggplot(mpg, aes(x = manufacturer)) +
   geom_histogram(stat = 'count')
 
-# Scatter Plots의 목적은 관계를 볼 때 사용된다
+# Scatter Plots의 목적은 관계를 볼 때 사용된다. 직접 그려보자
 # 간혹 이렇게 흩어지지 않을 때도 있다
 ggplot(mtcars, aes(x = cyl, y = wt)) +
   geom_point()
@@ -68,12 +68,88 @@ ggplot(diamonds, aes(x = carat, y = price)) +
 #------------------------------------------------
 # Bar Plots
 #------------------------------------------------
+install.packages("gcookbook")
+library(gcookbook)
 
+#기본적인 Bar chart 만들기
+ggplot(pg_mean, aes(x=group, y=weight)) + geom_bar(stat = "identity")
+ggplot(pg_mean, aes(x=group, y=weight)) + geom_bar(stat = "identity", fill = "#413FAC", colour="black")
 
+# group bar chart를 만들어보자
+ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + 
+  geom_bar(stat="identity",position="dodge")
+
+# position = "stack"
+ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + 
+  geom_bar(stat="identity",position="stack")
+
+# position = "fill"
+ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) + 
+  geom_bar(stat="identity",position="fill")
+
+### next, histogram
+
+# histogram을 그려보자
+str(diamonds)
+
+# 이산 변수 cut
+ggplot(diamonds, aes(x = cut)) +
+  geom_bar()
+
+# 이산 변수 color
+ggplot(diamonds, aes(x = color)) +
+  geom_bar()
+
+# 연속변수 depth를 geom_bar와 geom_histogram으로 비교해보자
+# 이산변수(geom_bar)는 각 값을 카운팅 한 것이고, 연속변수(geom_histogram)는 범위안에 들어가는 것을 카운팅
+ggplot(diamonds, aes(x = depth)) +
+  geom_bar()
+
+ggplot(diamonds, aes(x = depth)) +
+  geom_histogram() +
+  ylim(0, 25000)
+
+ggplot(diamonds, aes(x = depth)) +
+  geom_histogram(binwidth = 1.2) +
+  ylim(0, 25000)
+
+# binwidth = 4로 바꿔보자
+ggplot(diamonds, aes(x = depth)) +
+  geom_histogram(binwidth = 4)
+
+# 기본적으로 geom_histogram에서 binwidth를 계산하는 방식 (bins = 30이 default)
+diff(range(diamonds$depth)) / 30
+
+# fill로 구분해보자
+ggplot(diamonds, aes(price, fill = cut)) +
+  geom_histogram(binwidth = 500)
+
+# fill과 position을 함께 써보자
+ggplot(diamonds, aes(price, fill = cut)) +
+  geom_histogram(binwidth = 500, position = "dodge")
 
 #------------------------------------------------
 # Line Plots
 #------------------------------------------------
+# 내장데이터로 그려보자
+ggplot(BOD, aes(x = Time, y = demand)) + 
+  geom_line()
+
+# x축에 모든 x의 값이 표시됨, 6이 있나요?
+BOD$Time <- factor(BOD$Time)
+l1 <- ggplot(BOD, aes(x = Time, y = demand, group = 1)) + 
+  geom_line()
+
+# x축에 모든 x의 값이 표시됨, 6이 있나요?
+BOD$Time <- factor(BOD$Time)
+l2 <- ggplot(BOD, aes(x = Time, y = demand, group = 1)) + 
+  geom_line() +
+  ylim(0, max(BOD$demand))
+
+# y축의 범위가 달라지면 기울기도 달라진다. 유의할 것!
+gridExtra::grid.arrange(l1, l2, nrow = 1)
+
+##### next
 df_temp <- read_csv("data/line_temp.csv")
 
 # 1960년과 1994년 추출
